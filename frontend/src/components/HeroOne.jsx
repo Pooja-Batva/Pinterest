923383ba59f59263fc70f8935fd3c5052efd0a8e
Pooja-Batva <poojabatva2602@gmail.com>
2025-10-19 17:27:12 +0530

    Add Navbar component: implement layout with icons and search functionality

--- frontend/src/components/HeroOne.jsx
+++ frontend/src/components/HeroOne.jsx
@@ -8,9 +8,9 @@ function HeroOne() {
 
     return (
         <div className='flex w-full h-screen'>
-            <img src={welcomeimg} alt="image" className='absolute w-full h-full object-cover' />
+            <img src={welcomeimg} alt="image" className='absolute w-full h-full object-cover sm:hidden md:block' />
             <div className='absolute w-full h-full z-10 p-4 bg-black opacity-65'></div>
-            <div className='z-20 ml-20 mr-20 flex'>
+            <div className='z-20 ml-20 mr-20 flex flex-col sm:flex-row'>
                 <h1 className=' text-white text-4xl md:text-6xl font-bold mb-4 w-1/2 flex items-center pr-20'>Log in to get your ideas</h1>
                 <div className='flex justify-flex-end bg-white h-100vh'>
                     {!showSignup ? (
